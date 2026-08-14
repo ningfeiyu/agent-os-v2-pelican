78dcf855142018255121fc390913c56cd627322b
ningfeiyu <ningfeiyu@gmail.com>
2026-08-14 09:20:30 +0800

    Add pelicanconf.py

---
+++ pelicanconf.py
@@ -0,0 +1,13 @@
+AUTHOR = 'ling li'
+SITENAME = 'Agent OS v2.0 Blog'
+SITEURL = ''
+PATH = 'content'
+TIMEZONE = 'Asia/Shanghai'
+DEFAULT_LANG = 'zh'
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+DEFAULT_PAGINATION = 10
+THEME = 'simple'
